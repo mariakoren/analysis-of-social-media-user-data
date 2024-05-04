7131040fd02c0b995532f4a7ccf54af8fd938833
mariakoren <maria.korenn@gmail.com>
2024-05-04 20:34:08 +0200

    neural all fashion

--- neuralNetworkInterests.py
+++ neuralNetworkInterests.py
@@ -3,6 +3,10 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import FeatureHasher
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 data = pd.read_csv("DataCorrect.csv")
 
@@ -32,7 +36,12 @@ data = pd.concat([data.drop(columns=['City', 'Country']), hashed_df], axis=1)
 
 X = data.drop(columns=['Interests'])
 y = data['Interests']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+y = tf.keras.utils.to_categorical(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
@@ -43,10 +52,21 @@ model = tf.keras.models.Sequential([
 ])
 
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',  # Zmiana funkcji straty
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
 
 test_loss, test_acc = model.evaluate(X_test, y_test)
 print('Test accuracy:', test_acc)
+
+
+y_pred = model.predict(X_test)
+conf_matrix = confusion_matrix(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
+
+plt.figure(figsize=(100, 80))
+sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=interests, yticklabels=interests)
+plt.xlabel('Predicted labels')
+plt.ylabel('True labels')
+plt.title('Confusion Matrix')
+plt.savefig('confusion_matrix.png')
