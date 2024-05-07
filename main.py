fc338eec0e5f3ffe397f32b17d96cc81d348ead7
mariakoren <maria.korenn@gmail.com>
2024-05-07 14:45:28 +0200

    small changes

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ df = pd.read_csv("c:/Users/maria/Desktop/analysis-of-social-media-user-data/data
 df.drop(['Name', 'UserID'], axis=1, inplace=True)
 df.to_csv('DataWithNoNameNoId.csv', index=False)
 
-df = df.assign(**{'Interests': df['Interests'].str.split(',')}).explode('Interests')
+# df = df.assign(**{'Interests': df['Interests'].str.split(',')}).explode('Interests')
+# df.drop_duplicates(inplace=True)
+# df.to_csv('DataCorrect.csv', index=False)
+
+df['Interests'] = df['Interests'].str.split(',').str[0]
 df.drop_duplicates(inplace=True)
 df.to_csv('DataCorrect.csv', index=False)
